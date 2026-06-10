89909d77dfa19b1c89dad1556986389fad4e44c1
OptRay <42396587+optray@users.noreply.github.com>
2026-06-10 17:54:05 +0800

    Fix Kuramoto-Sivashinsky model imports

--- kuramoto_sivashinsky/neural_operator/model/__init__.py
+++ kuramoto_sivashinsky/neural_operator/model/__init__.py
@@ -1,22 +1,16 @@
-
 from .ffno import Model as FFNO
 from .cnext import Model as CNext
 from .factformer import Model as FactFormer
 from .percnn import Model as PeRCNN
 from .dpot import Model as DPOT
 from .ssno import Model as SSNO
-from .sino import Model as SINO
-from .sino2 import Model as SINO2
 
 
 model_dict = {
     'SSNO': SSNO,
-    'SINO': SINO,
-    'SINO2': SINO2,
     'FFNO': FFNO,
     'CNext': CNext,
     'FactFormer': FactFormer,
     'DPOT': DPOT,
     'PeRCNN': PeRCNN,
-    # ...
 }
